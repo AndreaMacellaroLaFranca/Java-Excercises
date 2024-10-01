63f3dab426ccdc64cae7100c6a45a9ef84914eea
Andrea Macellaro La Franca <andrea.macellaro@gmail.com>
2024-10-01 10:11:23 +0200

    Java practice first commit

---
+++ src/practice/pack/MethodsCalculator.java
@@ -0,0 +1,81 @@
+package practice.pack;
+
+import java.util.Scanner;
+
+public class MethodsCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Scegli il tipo di operazione: somma, sottrazione, moltiplicazione o divizione: ");
+        String operazione = sc.nextLine();
+
+        if (operazione.equals("somma")){
+
+            System.out.print("Inserire primo numero da sommare: ");
+            int a = sc.nextInt();
+
+            System.out.print("Inserire secondo numero da sommare: ");
+            int b = sc.nextInt();
+
+            System.out.print("Il risultato della somma e': " + somma(a,b));
+        }
+
+        if (operazione.equals("sottrazione")){
+
+            System.out.print("Inserire primo numero da sottrarre: ");
+            int a = sc.nextInt();
+
+            System.out.print("Inserire secondo numero da sottrarre: ");
+            int b = sc.nextInt();
+
+            System.out.print("Il risultato della sottrazione e': " + sottrazione(a,b));
+        }
+
+        if (operazione.equals("moltiplicazione")){
+
+            System.out.print("Inserire primo numero da moltiplicare: ");
+            int a = sc.nextInt();
+
+            System.out.print("Inserire secondo numero da moltiplicare: ");
+            int b = sc.nextInt();
+
+            System.out.print("Il risultato della moltiplicazione e': " + moltiplicazione(a,b));
+        }
+
+        if (operazione.equals("divisione")){
+
+            System.out.print("Inserire primo numero da dividere: ");
+            int a = sc.nextInt();
+
+            System.out.print("Inserire secondo numero da dividere: ");
+            int b = sc.nextInt();
+
+            System.out.print("Il risultato della divisione e': " + divisione(a,b));
+        }
+
+        sc.close();
+
+    }
+
+    public static int somma(int a,int b){
+        int res = a+b;
+        return res;
+    }
+
+    public static int sottrazione(int a,int b){
+        int res = a-b;
+        return res;
+    }
+
+    public static int moltiplicazione(int a,int b){
+        int res = a*b;
+        return res;
+    }
+
+    public static float divisione(float a,float b){
+        float res = a/b;
+        return res;
+    }
+
+}
