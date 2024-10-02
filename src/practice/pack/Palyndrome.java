b8b58ba056a8553fc54c21f86f538a409110bf7d
Andrea Macellaro La Franca <andrea.macellaro@gmail.com>
2024-10-02 10:04:40 +0200

    Java practice first commit

--- src/practice/pack/Palyndrome.java
+++ src/practice/pack/Palyndrome.java
@@ -7,25 +7,39 @@ public class Palyndrome {
 
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Inserisci una parola per verificare se sia palindroma: ");
-        String word = sc.nextLine();
 
-        System.out.println(palyndrome(word));
+        String word = "";
 
+            while (word!="stop") {
+                System.out.print("Inserisci una parola per verificare se sia palindroma o scrivi STOP per interrompere il programma: ");
+                word = sc.nextLine();
+                if (isPalyndrome(word)) {
+                    System.out.println("La parola '" + word + "' è un palindromo.");
+                } else if (word.equalsIgnoreCase("stop")) {
+                    System.out.println("PROGRAMMA INTERROTTO");
+                    return;
+                } else {
+                    System.out.println("La parola '" + word + "' non è un palindromo.");
+                }
+        }
+            sc.close();
     }
 
-    public static String palyndrome (String word){
-        String nonPalyndrome = "La parola "+word+" non e' palindroma";
+    public static boolean isPalyndrome (String word){
         int wordLength = word.length();
         String wordRev = "";
         for (int i = wordLength-1; i >= 0; i--) {
             wordRev += word.charAt(i);
         }
         if (wordRev.equalsIgnoreCase(word)){
-            return "La parola "+word+" e' palindroma";
+            return true;
         }else{
-            return nonPalyndrome;
+            return false;
         }
     }
 
 }
+
+
+
+
